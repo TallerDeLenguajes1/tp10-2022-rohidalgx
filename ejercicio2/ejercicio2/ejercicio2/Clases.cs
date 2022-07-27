337297c01388fed012007619daf3671f64c5d6bc
Rocio Hidalgo Bini <hidalgobinirocio@gmail.com>
2022-07-27 11:17:45 -0300

    terminado deshonestamente

---
+++ ejercicio2/ejercicio2/ejercicio2/Clases.cs
@@ -0,0 +1,112 @@
+﻿using System.Text.Json.Serialization;
+namespace TP102;
+
+public class Cost
+{
+    [JsonPropertyName("Wood")]
+    public int Wood { get; set; }
+
+    [JsonPropertyName("Stone")]
+    public int? Stone { get; set; }
+
+    [JsonPropertyName("Gold")]
+    public int? Gold { get; set; }
+}
+
+public class Estructuras
+{
+    [JsonPropertyName("structures")]
+    public List<Structure> lEstructuras { get; set; }
+}
+
+public class Structure
+{
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("expansion")]
+    public string Expansion { get; set; }
+
+    [JsonPropertyName("age")]
+    public string Age { get; set; }
+
+    [JsonPropertyName("cost")]
+    public Cost Cost { get; set; }
+
+    [JsonPropertyName("build_time")]
+    public int BuildTime { get; set; }
+
+    [JsonPropertyName("hit_points")]
+    public int HitPoints { get; set; }
+
+    [JsonPropertyName("line_of_sight")]
+    public int LineOfSight { get; set; }
+
+    [JsonPropertyName("armor")]
+    public string Armor { get; set; }
+
+    [JsonPropertyName("special")]
+    public List<string> Special { get; set; }
+
+    [JsonPropertyName("reload_time")]
+    public double? ReloadTime { get; set; }
+
+    [JsonPropertyName("range")]
+    public object Range { get; set; }
+
+    [JsonPropertyName("attack")]
+    public int? Attack { get; set; }
+}
+
+public class Jugador
+{
+
+    public int cantidadMadera;
+    public int cantidadOro;
+    public int cantidadPiedra;
+    public List<Structure> misEstructuras;
+
+    public Jugador(int cantidadM, int cantidadO, int cantidadP)
+    {
+        this.cantidadMadera = cantidadM;
+        this.cantidadOro = cantidadO;
+        this.cantidadPiedra = cantidadP;
+        misEstructuras = new List<Structure>();
+    }
+
+    public void agregarEstructura(Structure estructura)
+    {
+        misEstructuras.Add(estructura);
+        this.cantidadMadera -= estructura.Cost.Wood;
+        this.cantidadOro -= estructura.Cost.Gold ?? 0;
+        this.cantidadPiedra -= estructura.Cost.Stone ?? 0;
+    }
+
+    public void mostrarMisEstructuras()
+    {
+        if (misEstructuras.Any())
+        {
+            Console.WriteLine("--> Tus estructuras: ");
+            foreach (var estructura in misEstructuras)
+            {
+                Console.WriteLine("  - " + estructura.Name);
+            }
+        }
+        else
+        {
+            Console.WriteLine("No tienes estructuras.");
+        }
+    }
+
+    public void mostrarRecursos()
+    {
+        Console.WriteLine("--> Tus recursos: ");
+        Console.WriteLine(" - ORO: " + this.cantidadOro);
+        Console.WriteLine(" - MADERA: " + this.cantidadMadera);
+        Console.WriteLine(" - PIEDRA: " + this.cantidadPiedra);
+    }
+
+}
---
+++ ejercicio2/ejercicio2/ejercicio2/Program.cs
@@ -0,0 +1,141 @@
+﻿using System.Text.Json;
+using System.Net;
+namespace TP102;
+
+class Program
+{
+    static int Main(string[] args)
+    {
+
+        Console.WriteLine("======= ESTRUCTURAS AoE2 =======");
+        Estructuras listaEstructuras = ObtenerEstructuras();
+
+        if (listaEstructuras.lEstructuras.Any())
+        {
+            Console.WriteLine("Hola");
+            AdministradorEstructuras(listaEstructuras);
+        }
+
+        return 0;
+
+    }
+
+    public static Estructuras ObtenerEstructuras()
+    {
+
+        Estructuras listaEstructuras = new Estructuras();
+
+        string url = $"https://age-of-empires-2-api.herokuapp.com/api/v1/structures";
+        var request = (HttpWebRequest)WebRequest.Create(url);
+
+        request.Method = "GET";
+        request.Accept = "application/json";
+        request.ContentType = "application/json";
+
+        try
+        {
+            using (WebResponse respuesta = request.GetResponse())
+            {
+                using (Stream reader = respuesta.GetResponseStream())
+                {
+                    if (reader != null)
+                    {
+
+                        using (StreamReader objectReader = new StreamReader(reader))
+                        {
+                            string lecturaJson = objectReader.ReadToEnd();
+                            listaEstructuras = JsonSerializer.Deserialize<Estructuras>(lecturaJson);
+                        }
+                    }
+                }
+            }
+        }
+        catch (WebException excepcion)
+        {
+            Console.WriteLine("Hubo un error al conectar con el servicio web.");
+        }
+
+        return listaEstructuras;
+
+    }
+
+    public static void AdministradorEstructuras(Estructuras listaEstructuras)
+    {
+
+        Random rand = new Random();
+        Jugador J1 = new Jugador(rand.Next(1000), rand.Next(1500), rand.Next(1000));
+
+        int operacion = 0;
+
+        do
+        {
+            Console.WriteLine("== ADMINISTRADOR DE ESTRUCTURAS: ");
+
+            do
+            {
+                Console.WriteLine(" 1) Ver recursos ");
+                Console.WriteLine(" 2) Ver estructuras ");
+                Console.WriteLine(" 3) Comprar estructura ");
+                Console.WriteLine(" 4) Salir ");
+                int.TryParse(Console.ReadLine(), out operacion);
+
+            } while (operacion < 1 || operacion > 4);
+
+            switch (operacion)
+            {
+                case 1:
+                    J1.mostrarRecursos();
+                    break;
+                case 2:
+                    J1.mostrarMisEstructuras();
+                    break;
+                case 3:
+                    ComprarEstructuras(J1, listaEstructuras);
+                    break;
+            }
+
+        } while (operacion != 4);
+    }
+
+    public static void ComprarEstructuras(Jugador J, Estructuras listaEstructuras)
+    {
+
+        int i = 1;
+        foreach (var estructura in listaEstructuras.lEstructuras)
+        {
+
+            string infoEstructura = $"{i}) {estructura.Name} - Costo: ";
+
+            if (estructura.Cost.Gold != null) infoEstructura += $"{estructura.Cost.Gold} oro, ";
+            if (estructura.Cost.Wood != null) infoEstructura += $"{estructura.Cost.Wood} madera, ";
+            if (estructura.Cost.Stone != null) infoEstructura += $"{estructura.Cost.Stone} piedra.";
+
+            Console.WriteLine(infoEstructura);
+
+            i++;
+        }
+
+        int estructuraElegida = 0;
+
+        do
+        {
+            Console.WriteLine("¿Cuál estructura desea comprar?");
+            int.TryParse(Console.ReadLine(), out estructuraElegida);
+
+        } while (estructuraElegida < 1 || estructuraElegida >= i);
+
+        int cantidadOroEstructuraElegida = listaEstructuras.lEstructuras[estructuraElegida - 1].Cost.Gold ?? 0;
+        int cantidadMaderaEstructuraElegida = listaEstructuras.lEstructuras[estructuraElegida - 1].Cost.Wood;
+        int cantidadPiedraEstructuraElegida = listaEstructuras.lEstructuras[estructuraElegida - 1].Cost.Stone ?? 0;
+
+        if (cantidadOroEstructuraElegida <= J.cantidadOro && cantidadMaderaEstructuraElegida <= J.cantidadMadera && cantidadPiedraEstructuraElegida <= J.cantidadPiedra)
+        {
+            J.agregarEstructura(listaEstructuras.lEstructuras[estructuraElegida - 1]);
+        }
+        else
+        {
+            Console.WriteLine(" -> Recursos insuficientes :(");
+        }
+
+    }
+}
